603a1cb5dbc90730954a5b6d2d07d12bd79b495b
Vincent Fong <vincentfong@Vincents-MacBook-Pro.local>
2018-11-11 14:34:24 -0500

    fixed bugs, wrote new constructor

--- DriverV.java
+++ DriverV.java
@@ -1,46 +1,7 @@
 public class DriverV {
   public static void main(String[] args) {
-    System.out.println("\n1. CONSTRUCTOR INITIALIZATION + TOSTRING");
-    // System.out.println("Creating a -1 x -1 WordSearch (should fail, print that it caught an exception )");
-    System.out.println("Creating a 5 x 5 WordSearch and printing (hopefully y'all know what that is supposed to look like)");
-    WordSearch WS = new WordSearch(5,5);
+    WordSearch WS = new WordSearch(5,5,"words.txt");
+    WS.addWord(2,2,"abc",-1,1);
     System.out.println(WS);
-    System.out.println("\n---------------\n");
-
-    System.out.println("2. ADD WORD HORIZONTAL");
-    System.out.println("Adding to a negative row index (should print false): " + WS.addWordHorizontal("abc",-1,2));
-    System.out.println("Adding to a negative column index (should print false): " + WS.addWordHorizontal("abc",0,-1));
-    System.out.println("Adding to an out of bounds row index (should print false): " + WS.addWordHorizontal("abc",10,0));
-    System.out.println("Adding to an out of bounds column index (should print false): " + WS.addWordHorizontal("abc",0,10));
-    System.out.println("Adding to last index of row, where a 3-letter word doesn't fit (should print false): " + WS.addWordHorizontal("abc",0,4));
-    System.out.println("Adding a 6-letter word (doesn't fit, should print false): " + WS.addWordHorizontal("abcdef",0,4));
-    System.out.println("Adding \"abc\" to last 3 slots of each row (should print true, once): " + (WS.addWordHorizontal("abc",0,2) && WS.addWordHorizontal("abc",1,2) && WS.addWordHorizontal("abc",2,2) && WS.addWordHorizontal("abc",3,2) && WS.addWordHorizontal("abc",4,2)));
-    System.out.println("\nPrinting WS (see above description)\n" + WS);
-    System.out.println("\n---------------\n");
-
-    System.out.println("3. ADD WORD VERTICAL (with a new WordSearch! of the same dimensions!)");
-    WordSearch SW = new WordSearch(5,5);
-    System.out.println("Adding to a negative row index (should print false): " + SW.addWordVertical("abc",-1,2));
-    System.out.println("Adding to a negative column index (should print false): " + SW.addWordVertical("abc",0,-1));
-    System.out.println("Adding to an out of bounds row index (should print false): " + SW.addWordVertical("abc",10,0));
-    System.out.println("Adding to an out of bounds column index (should print false): " + SW.addWordVertical("abc",0,10));
-    System.out.println("Adding to last index of column, where a 3-letter word doesn't fit (should print false): " + SW.addWordVertical("abc",4,0));
-    System.out.println("Adding a 6-letter word (doesn't fit, should print false): " + SW.addWordHorizontal("abcdef",0,0));
-    System.out.println("Adding \"abc\" to last 3 slots of each column (should print true, once): " + (SW.addWordVertical("abc",2,0) && SW.addWordVertical("abc",2,1) && SW.addWordVertical("abc",2,2) && SW.addWordVertical("abc",2,3) && SW.addWordVertical("abc",2,4)));
-    System.out.println("\nPrinting WS (see above description)\n" + SW);
-    System.out.println("\n---------------\n");
-
-    System.out.println("3. ADD WORD DIAGONAL (with a new WordSearch! of the same dimensions!)");
-    WordSearch SWW = new WordSearch(5,5);
-    System.out.println("Adding to a negative row index (should print false): " + SWW.addWordDiagonal("abc",-1,2));
-    System.out.println("Adding to a negative column index (should print false): " + SWW.addWordDiagonal("abc",0,-1));
-    System.out.println("Adding to an out of bounds row index (should print false): " + SWW.addWordDiagonal("abc",10,0));
-    System.out.println("Adding to an out of bounds column index (should print false): " + SWW.addWordDiagonal("abc",0,10));
-    System.out.println("Adding to last index of row, where a 3-letter word doesn't fit (should print false): " + SWW.addWordDiagonal("abc",0,4));
-    System.out.println("Adding to last index of column, where a 3-letter word doesn't fit (should print false): " + SWW.addWordDiagonal("abc",4,0));
-    System.out.println("Adding a 6-letter word (doesn't fit, should print false): " + SWW.addWordDiagonal("abcdef",0,0));
-    System.out.println("Adding numbers to each diagonal (should print true, once): " + (SWW.addWordDiagonal("0",4,0) && SWW.addWordDiagonal("11",3,0) && SWW.addWordDiagonal("222",2,0) && SWW.addWordDiagonal("3333",1,0) && SWW.addWordDiagonal("44444",0,0) && SWW.addWordDiagonal("3333",0,1) && SWW.addWordDiagonal("222",0,2) && SWW.addWordDiagonal("222",0,2) && SWW.addWordDiagonal("11",0,3) && SWW.addWordDiagonal("0",0,4)));
-    System.out.println("\nPrinting WS (see above description)\n" + SWW);
-    System.out.println("\n---------------\n");
   }
 }
--- WordSearch.java
+++ WordSearch.java
@@ -1,8 +1,9 @@
 import java.util.*; //random, scanner, arraylist
 import java.io.*; //file, filenotfoundexception
-public class WordSearch{
+public class WordSearch {
     private char[][]data;
-
+    private int width;
+    private int height;
     //the random seed used to produce this WordSearch
     private int seed;
     //a random Object to unify your random calls
@@ -17,27 +18,40 @@ public class WordSearch{
      *@param row is the starting height of the WordSearch
      *@param col is the starting width of the WordSearch
      */
-    public WordSearch(int rows,int cols) {
+    public WordSearch(int rows,int cols, String filename) {
       if ((rows > 0) && (cols > 0)) {
         data = new char[rows][cols];
+        height = this.data.length;
+        width = this.data[0].length;
+        wordsToAdd = new ArrayList<String>();
+        wordsAdded = new ArrayList<String>();
+        seed = (int) System.currentTimeMillis();
+        randgen = new Random();
         this.clear();
+
+        try {
+          File file = new File(filename);
+          Scanner sc = new Scanner(file);
+
+          while (sc.hasNext()) {
+            wordsToAdd.add(sc.nextLine());
+          }
+        } catch (FileNotFoundException e) {
+          System.out.println("File not found: " + filename);
+          e.printStackTrace();
+          System.exit(1);
+        }
+
+
       } else {
         throw new IllegalArgumentException("error, negative index(es)");
       }
     }
 
-    public int rowLength() {
-      return this.data.length;
-    }
-
-    public int colLength() {
-      return this.data[0].length;
-    }
-
     /**Set all values in the WordSearch to underscores'_'*/
     private void clear() {
-      for (int i = 0; i < this.data.length; i++) {
-        for (int j = 0; j < this.data[i].length; j++) {
+      for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
           this.data[i][j] = '_';
         }
       }
@@ -49,23 +63,29 @@ public class WordSearch{
      */
     public String toString() {
       String ans = "";
-      for (int i = 0; i < this.data.length; i++) {
-        for (int j = 0; j < this.data[i].length; j++) {
-          ans += this.data[i][j] + " ";
+      for (int i = 0; i < height; i++) {
+        ans += "|";
+        for (int j = 0; j < width; j++) {
+          ans += this.data[i][j];
+          if (j != width - 1) {
+            ans += " ";
+          }
         }
-        if (i != this.data.length - 1) {
-          ans += "\n";
+        if (i != height - 1) {
+          ans += "|\n";
+        } else {
+          ans += "|";
         }
       }
       return ans;
     }
 
-    private boolean addWord(int r, int c, String word, int rowIncrement, int colIncrement) {
+    public boolean addWord(int r, int c, String word, int rowIncrement, int colIncrement) {
       if ((rowIncrement == 0) && (colIncrement == 0)) {
         return false;
       } else {
         int wordLength = word.length();
-        if ((r >= 0) && (c >= 0) && ((r + (rowIncrement * wordLength)) <= rowLength()) && ((r + (rowIncrement * wordLength)) >= -1) && ((c + (colIncrement * wordLength)) <= colLength()) && ((c + (colIncrement * wordLength)) >= -1)) {
+        if ((r >= 0) && (c >= 0) && ((r + (rowIncrement * wordLength)) <= width) && ((r + (rowIncrement * wordLength)) >= -1) && ((c + (colIncrement * wordLength)) <= height) && ((c + (colIncrement * wordLength)) >= -1)) {
           for(int i = 0; i < wordLength; i++) {
             this.data[r + (i * rowIncrement)][c + (i * colIncrement)] = word.charAt(i);
           }
@@ -74,71 +94,4 @@ public class WordSearch{
         return false;
       }
     }
-   //  /**Attempts to add a given word to the specified position of the WordGrid.
-   //   *The word is added from left to right, must fit on the WordGrid, and must
-   //   *have a corresponding letter to match any letters that it overlaps.
-   //   *
-   //   *@param word is any text to be added to the word grid.
-   //   *@param row is the vertical locaiton of where you want the word to start.
-   //   *@param col is the horizontal location of where you want the word to start.
-   //   *@return true when the word is added successfully. When the word doesn't fit,
-   //   * or there are overlapping letters that do not match, then false is returned
-   //   * and the board is NOT modified.
-   //   */
-   //  public boolean addWordHorizontal(String word,int row, int col) {
-   //    int wordLength = word.length();
-   //    if ((row < this.rowLength()) && (row >= 0) && ((wordLength + col) <= this.columnLength()) && (col >= 0)) {
-   //      for(int i = col; i < (col + wordLength); i++) {
-   //        this.data[row][i] = word.charAt(i - col);
-   //      }
-   //      return true;
-   //    } else {
-   //      return false;
-   //    }
-   //  }
-   //
-   // /**Attempts to add a given word to the specified position of the WordGrid.
-   //   *The word is added from top to bottom, must fit on the WordGrid, and must
-   //   *have a corresponding letter to match any letters that it overlaps.
-   //   *
-   //   *@param word is any text to be added to the word grid.
-   //   *@param row is the vertical locaiton of where you want the word to start.
-   //   *@param col is the horizontal location of where you want the word to start.
-   //   *@return true when the word is added successfully. When the word doesn't fit,
-   //   *or there are overlapping letters that do not match, then false is returned.
-   //   *and the board is NOT modified.
-   //   */
-   //  public boolean addWordVertical(String word,int row, int col) {
-   //    int wordLength = word.length();
-   //    if ((col < this.columnLength()) && (col >= 0) && ((wordLength + row) <= this.rowLength()) && (row >= 0)) {
-   //      for(int i = row; i < (row + wordLength); i++) {
-   //        this.data[i][col] = word.charAt(i - row);
-   //      }
-   //      return true;
-   //    } else {
-   //      return false;
-   //    }
-   //  }
-   //
-   //  /**Attempts to add a given word to the specified position of the WordGrid.
-   //   *The word is added from top left to bottom right, must fit on the WordGrid,
-   //   *and must have a corresponding letter to match any letters that it overlaps.
-   //   *
-   //   *@param word is any text to be added to the word grid.
-   //   *@param row is the vertical locaiton of where you want the word to start.
-   //   *@param col is the horizontal location of where you want the word to start.
-   //   *@return true when the word is added successfully. When the word doesn't fit,
-   //   *or there are overlapping letters that do not match, then false is returned.
-   //   */
-   //  public boolean addWordDiagonal(String word,int row, int col) {
-   //    int wordLength = word.length();
-   //    if (((wordLength + col) <= this.columnLength()) && (col >= 0) && ((wordLength + row) <= this.rowLength()) && (row >= 0)) {
-   //      for(int i = 0; i < wordLength; i++) {
-   //        this.data[row + i][col + i] = word.charAt(i);
-   //      }
-   //      return true;
-   //    } else {
-   //      return false;
-   //    }
-   //  }
 }
