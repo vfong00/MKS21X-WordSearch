4369c8d0e527e769b65bf23aaaee4c3a1bc22879
Vincent Fong <vfong00@stuy.edu>
2018-11-11 23:48:02 -0500

    integrated terminal args

--- DriverV.java
+++ DriverV.java
@@ -1,7 +1,17 @@
 public class DriverV {
   public static void main(String[] args) {
-    WordSearch WS = new WordSearch(5,5,"words.txt");
-    WS.addWord(2,2,"abc",-1,1);
-    System.out.println(WS);
+    try {
+      if (args.length == 3) {
+        WordSearch WS = new WordSearch(Integer.parseInt(args[0]),Integer.parseInt(args[1]),args[2]);
+        System.out.println(WS);
+      } else if (args.length == 4) {
+        WordSearch WS = new WordSearch(Integer.parseInt(args[0]),Integer.parseInt(args[1]),args[2],Integer.parseInt(args[3]));
+        System.out.println(WS);
+      } else {
+        System.out.println("incorrect number of args given, must be:\nint rows, int columns, textfile of words, (optional) int seed");
+      }
+    } catch (NumberFormatException e) {
+      System.out.println("invalid format of args given, must be:\nint rows, int columns, textfile of words, (optional) int seed");
+    }
   }
 }
